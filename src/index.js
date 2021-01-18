8aa1c10edd1c86dd313402d3a26088d0a94160b8
Nathan <nmf2@cin.ufpe.br>
2021-01-18 03:02:52 -0300

    feat: swagger documentation UI

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import setupDatabase from "@/config/database";
 import express from "express";
 import routes from "./routes";
 import * as bodyParser from "body-parser";
-// import { middleware as ValidatorOpenApi } from "express-openapi-validator";
+import { middleware as ValidatorOpenApi } from "express-openapi-validator";
+import path from "path";
+// import swaggerUi from "swagger-ui-express";
+import fs from 'fs'
+import YAML from "yamljs";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -16,6 +20,28 @@ app.use(
     limit: process.env.REQUEST_LIMIT || "800kb",
   })
 );
+
+const apiSpec = path.join(__dirname, "./api.yaml");
+console.log(`Spec`)
+const swaggerDocument = YAML.load(apiSpec);
+
+app.use(express.static(`public`))
+fs.writeFileSync(apiSpec, JSON.stringify(swaggerDocument))
+app.use(process.env.OPENAPI_SPEC || '/spec', express.static(apiSpec))
+
+app.use(
+  ValidatorOpenApi({
+    apiSpec,
+    validateRequests: true,
+  }),
+  (err, req, res, next) => {
+    res.status(err.status || 500).json({
+      message: err.message,
+      errors: err.errors,
+    });
+  }
+);
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, {  }));
 
 routes(app);
 
